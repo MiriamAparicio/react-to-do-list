7416b32f434e2e20ee63282a8cb464cb843a6ed0
Miriam Aparicio <miriam.aparicio@gmail.com>
2018-06-07 10:39:28 +0200

    todo-list done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import Title from './components/Title';
+import ToDoForm from './components/ToDoForm';
+import ToDoList from './components/ToDoList';
+
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor(props) {
+    // Pass props to parent class
+    super(props);
+    // Set initial state
+    this.state = {
+      data: []
+    }
+    this.apiUrl = 'https://57b1924b46b57d1100a3c3f8.mockapi.io/api/todos'
+  }
+
+  // Lifecycle method
+  componentDidMount() {
+    // Make HTTP reques with Axios
+    axios.get(this.apiUrl)
+      .then((res) => {
+        // Set state with result
+        this.setState({ data: res.data });
+      });
+  }
+  // Add todo handler
+  addTodo(val) {
+    // Assemble data
+    const todo = { text: val }
+    // Update data
+    axios.post(this.apiUrl, todo)
+      .then((res) => {
+        this.state.data.push(res.data);
+        this.setState({ data: this.state.data });
+      });
+  }
+  // Handle remove
+  handleRemove(id) {
+    // Filter all todos except the one to be removed
+    const remainder = this.state.data.filter((todo) => {
+      if (todo.id !== id) return todo;
+    });
+    // Update state with filter
+    axios.delete(this.apiUrl + '/' + id)
+      .then((res) => {
+        this.setState({ data: remainder });
+      })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +61,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React To-Do-List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="container">
+          <Title todoCount={this.state.data.length} />
+          <ToDoForm addTodo={this.addTodo.bind(this)} />
+          <ToDoList
+            todos={this.state.data}
+            remove={this.handleRemove.bind(this)}
+          />
+        </div>
       </div>
     );
   }
